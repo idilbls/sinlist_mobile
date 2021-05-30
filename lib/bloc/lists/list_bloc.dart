ce7cf3fe887e02b63b5c0602b8ce4431eecc3157
Ehlullah Karakurt <ehlullah.karakurt@inksen.com>
2021-05-30 14:22:29 +0300

    added comment lines

--- lib/bloc/lists/list_bloc.dart
+++ lib/bloc/lists/list_bloc.dart
@@ -1,63 +1,63 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class ListBloc extends Bloc<TodoEvent,TodoState>{
-  TodoRepository todoRepo = TodoRepository();
-  @override
-  TodoState get initialState => InitialState();
-
-  @override
-  Stream<TodoState> mapEventToState(TodoEvent event) async*{
-
-    try{
-      //Fetch All Todos
-      if(event is FetcheTodos){
-        yield RequestingState();
-        List<Todo> todoList = await todoRepo.GetTodos("");
-        yield FetchedTodosState(todos: todoList);
-      }
-      //Add New Todo
-      if(event is AddNewTodo){
-        yield RequestingState();
-        await todoRepo.PostTodo(event.todo);
-        yield RequestingState();
-        List<Todo> todoList = await todoRepo.GetTodos("");
-        yield FetchedTodosState(todos: todoList);
-      }
-
-      //Update Todo
-      if(event is UpdateTodo){
-
-        print('${event.todoId } : ${ event.todo}');
-
-        yield RequestingState();
-        await todoRepo.UpdateTodo(event.todoId,event.todo);
-        yield RequestingState();
-        List<Todo> todoList = await todoRepo.GetTodos("");
-        yield FetchedTodosState(todos: todoList);
-      }
-      //Delete Todo
-      if(event is DeleteTodo){
-
-        print('${event.todoId }');
-
-        yield RequestingState();
-        await todoRepo.DeleteTodo(event.todoId);
-        yield RequestingState();
-        List<Todo> todoList = await todoRepo.GetTodos("");
-        yield FetchedTodosState(todos: todoList);
-      }
-      //Search Todos
-      //Fetch All Todos
-      if(event is SearchTodos){
-        yield RequestingState();
-        print(event.search);
-        List<Todo> todoList = await todoRepo.GetTodos(event.search);
-        yield FetchedTodosState(todos: todoList);
-      }
-    }catch(error){
-      yield ErrorState(msg: error.toString());
-    }
-
-  }
-
-}
+// import 'package:flutter_bloc/flutter_bloc.dart';
+//
+// class ListBloc extends Bloc<TodoEvent,TodoState>{
+//   TodoRepository todoRepo = TodoRepository();
+//   @override
+//   TodoState get initialState => InitialState();
+//
+//   @override
+//   Stream<TodoState> mapEventToState(TodoEvent event) async*{
+//
+//     try{
+//       //Fetch All Todos
+//       if(event is FetcheTodos){
+//         yield RequestingState();
+//         List<Todo> todoList = await todoRepo.GetTodos("");
+//         yield FetchedTodosState(todos: todoList);
+//       }
+//       //Add New Todo
+//       if(event is AddNewTodo){
+//         yield RequestingState();
+//         await todoRepo.PostTodo(event.todo);
+//         yield RequestingState();
+//         List<Todo> todoList = await todoRepo.GetTodos("");
+//         yield FetchedTodosState(todos: todoList);
+//       }
+//
+//       //Update Todo
+//       if(event is UpdateTodo){
+//
+//         print('${event.todoId } : ${ event.todo}');
+//
+//         yield RequestingState();
+//         await todoRepo.UpdateTodo(event.todoId,event.todo);
+//         yield RequestingState();
+//         List<Todo> todoList = await todoRepo.GetTodos("");
+//         yield FetchedTodosState(todos: todoList);
+//       }
+//       //Delete Todo
+//       if(event is DeleteTodo){
+//
+//         print('${event.todoId }');
+//
+//         yield RequestingState();
+//         await todoRepo.DeleteTodo(event.todoId);
+//         yield RequestingState();
+//         List<Todo> todoList = await todoRepo.GetTodos("");
+//         yield FetchedTodosState(todos: todoList);
+//       }
+//       //Search Todos
+//       //Fetch All Todos
+//       if(event is SearchTodos){
+//         yield RequestingState();
+//         print(event.search);
+//         List<Todo> todoList = await todoRepo.GetTodos(event.search);
+//         yield FetchedTodosState(todos: todoList);
+//       }
+//     }catch(error){
+//       yield ErrorState(msg: error.toString());
+//     }
+//
+//   }
+//
+// }
